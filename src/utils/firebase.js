85dd41279cccec54f230a03dfbfc64063da5b46f
Chiew Carol <kyaroru@outlook.com>
2017-07-17 22:39:19 +0800

    prevent firebase initialize more than once

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,13 +4,16 @@ let firebaseApp;
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAgdNvb5RFt0-z-JzoLgH1SM_59nSY98Is',
-  // authDomain: "<your-auth-domain>",
   databaseURL: 'https://rnchat-59a49.firebaseio.com',
   storageBucket: 'rnchat-59a49.appspot.com',
 };
 
 export const initializeFirebaseApp = () => {
-  firebaseApp = firebase.initializeApp(firebaseConfig);
+  if (firebase.apps.length) {
+    console.log('[Firebase] Already Initialized');
+  } else {
+    firebaseApp = firebase.initializeApp(firebaseConfig);
+  }
 };
 
 export const getFirebaseApp = () => firebaseApp;
